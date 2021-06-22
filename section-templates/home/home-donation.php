6ddddb846655ef2df6e9d5d764079249ece4e0df
Amri Karisma <amrikarisma@yahoo.com>
2021-06-23 02:02:05 +0700

    update donation home

--- section-templates/home/home-donation.php
+++ section-templates/home/home-donation.php
@@ -1,69 +1,47 @@
 <section class="donation">
     <div class="donation-header">
         <h2>Latest Causes</h2>
-        <div class="tagline"><p>Organization set up to provide help and raise money for those in need</p></div>
+        <div class="tagline">
+            <p>Organization set up to provide help and raise money for those in need</p>
+        </div>
     </div>
     <div class="wrap-list-donation">
         <div class="container">
             <div class="row">
-                
-                <div class="col-md-4">
-                    <div class="wrap-donation-image">
-                        <img src="https://goodwish.qodeinteractive.com/elementor/wp-content/uploads/2017/03/h1-img-4.jpg" alt="">
-                    </div>
-                    <div class="wrap-donation-text">
-                        <div class="category"><a href="#">Healthcare</a></div>
-                        <h2>Places To Get Lost</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit hendrerit faucibus. Suspendisse hendrerit turpis dui, eget ultricies erat consequat ut. Sed ac velit iaculis, condimentum neqlu.</p>
-                        <div class="wrap-progress-bar">
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" style="width: 56%" aria-valuenow="56" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <div class="d-flex justify-content-between mt-3">
-                                <div>Raised: <strong>$51,869</strong></div>
-                                <div>Goal: <strong>$70,000</strong></div>
+                <?php
+                $args = array(
+                    'post_type'      => 'donasi',
+                    'posts_per_page' => 10,
+                );
+                $loop = new WP_Query($args);
+                while ($loop->have_posts()) {
+                    $loop->the_post();
+                ?>
+                    <div class="col-md-4 mb-4">
+                        <div class="wrap-donation-image">
+                            <?php the_post_thumbnail(); ?>
+                        </div>
+                        <div class="wrap-donation-text">
+                            <div class="category"><?php the_category(); ?></div>
+                            <h2><?php the_title(); ?></h2>
+                            <p><?php the_excerpt(); ?></p>
+                            <div class="wrap-progress-bar">
+                                <div class="progress">
+                                    <div class="progress-bar" role="progressbar" style="width: 56%" aria-valuenow="56" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <div class="d-flex justify-content-between mt-3">
+                                    <div>Raised: <strong>Rp. 123.000</strong></div>
+                                    <div>Goal: <strong>Rp. 15.000.000</strong></div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="wrap-donation-image">
-                        <img src="https://goodwish.qodeinteractive.com/elementor/wp-content/uploads/2017/03/h1-img-4.jpg" alt="">
-                    </div>
-                    <div class="wrap-donation-text">
-                        <div class="category"><a href="#">Healthcare</a></div>
-                        <h2>Places To Get Lost</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit hendrerit faucibus. Suspendisse hendrerit turpis dui, eget ultricies erat consequat ut. Sed ac velit iaculis, condimentum neqlu.</p>
-                        <div class="wrap-progress-bar">
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" style="width: 27%" aria-valuenow="27" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <div class="d-flex justify-content-between mt-3">
-                                <div>Raised: <strong>$51,869</strong></div>
-                                <div>Goal: <strong>$70,000</strong></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="wrap-donation-image">
-                        <img src="https://goodwish.qodeinteractive.com/elementor/wp-content/uploads/2017/03/h1-img-4.jpg" alt="">
-                    </div>
-                    <div class="wrap-donation-text">
-                        <div class="category"><a href="#">Healthcare</a></div>
-                        <h2>Places To Get Lost</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit hendrerit faucibus. Suspendisse hendrerit turpis dui, eget ultricies erat consequat ut. Sed ac velit iaculis, condimentum neqlu.</p>
-                        <div class="wrap-progress-bar">
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" style="width: 83%" aria-valuenow="83" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <div class="d-flex justify-content-between mt-3">
-                                <div>Raised: <strong>$51,869</strong></div>
-                                <div>Goal: <strong>$70,000</strong></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php
+                }
+
+                ?>
+
+
             </div>
         </div>
     </div>
