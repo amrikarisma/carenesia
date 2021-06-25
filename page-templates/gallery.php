fe49b97e9ec77e4168f22ff282dbf2790ef8e98e
Amri Karisma <amrikarisma@yahoo.com>
2021-06-26 01:27:01 +0700

    update css mobile + add gallery

---
+++ page-templates/gallery.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Template Name: Gallery Template
+ *
+ * Template for displaying a page just with the header and footer area and a "naked" content area in between.
+ * Good for landingpages and other types of pages where you want to add a lot of custom markup.
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined('ABSPATH') || exit;
+
+get_header();
+
+
+while (have_posts()) :
+	the_post();
+	get_template_part('section-templates/general/general', 'header');
+	get_template_part('section-templates/gallery/gallery', 'list');
+endwhile;
+
+get_footer();
