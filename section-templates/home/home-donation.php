c420747fbcc9215c2e8093b59c89214ac69ee193
Amri Karisma <amrikarisma@yahoo.com>
2021-06-29 00:27:15 +0700

    4by3

--- section-templates/home/home-donation.php
+++ section-templates/home/home-donation.php
@@ -28,7 +28,7 @@
                     <div class="col-md-6 col-lg-4 mb-4">
                         <a class="list-donation-box" href="<?php the_permalink(); ?>">
                             <div class="wrap-donation-image">
-                                <div class="embed-responsive embed-responsive-16by9">
+                                <div class="embed-responsive embed-responsive-4by3">
                                     <div class="embed-responsive-item">
                                         <?php the_post_thumbnail(); ?>
                                     </div>
